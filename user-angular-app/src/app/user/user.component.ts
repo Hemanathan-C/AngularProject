46b5143d75b1b139ca25c3b8ed3da44b7a44b5be
Hemanathan C <108792959+Hemanathan-C@users.noreply.github.com>
2025-06-23 07:31:40 +0530

    user image binding

--- user-angular-app/src/app/user/user.component.ts
+++ user-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from './dummy-users';
+
+
+const randomeIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,5 +11,10 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+    user = DUMMY_USERS[randomeIndex];
+    userImg = `assets/users/${this.user.avatar}`;
 
+    get imagePath() {
+        return 'assets/users/' + this.user.avatar;
+    }
 }
