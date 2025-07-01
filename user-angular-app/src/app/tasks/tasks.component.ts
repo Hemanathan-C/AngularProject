3f0a4bf63e868f170e1c3d023fe019810db26884
Hemanathan C <108792959+Hemanathan-C@users.noreply.github.com>
2025-07-01 22:20:44 +0530

    added functionality for cancel button

--- user-angular-app/src/app/tasks/tasks.component.ts
+++ user-angular-app/src/app/tasks/tasks.component.ts
@@ -2,10 +2,11 @@ import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../user/dummy-users'; 
 import { TaskComponent } from './task/task.component';
 import { DUMMY_TASKS } from './task/dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent,NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css'] 
 })
@@ -13,6 +14,7 @@ export class TasksComponent {
   @Input() name!: string;
   @Input() id!: string;
   tasks = DUMMY_TASKS;
+  isAddTask = false;
   
   get selectedUserTask(){
     console.log('APP Selected USER :',this.tasks.find((user) => user.id === this.id)!);
@@ -24,5 +26,13 @@ export class TasksComponent {
   onTaskCompleted(taskId: string) {
     console.log('Task completed:', taskId);
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+  }
+
+  onAddTask() {
+    this.isAddTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddTask = false;
   }
 }
