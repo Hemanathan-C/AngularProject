4546558af02523d69609718da0a717f6f3c72c70
Hemanathan C <108792959+Hemanathan-C@users.noreply.github.com>
2025-07-05 22:32:41 +0530

    structure changed to NG module

--- user-angular-app/src/app/app.component.ts
+++ user-angular-app/src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './user/dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: false
 })
 export class AppComponent {
   title = 'user-angular-app';
