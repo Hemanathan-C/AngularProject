128ac7875d3beb17fa4394446b648d1e84e02398
Hemanathan C <108792959+Hemanathan-C@users.noreply.github.com>
2025-07-08 22:40:21 +0530

    added ngmodel and ngsubmit

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,HeaderComponent,UserInputComponent],
+  imports: [HeaderComponent,UserInputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Investment Calculator';
+  calculateInvestmentResults(data: {initialInvestment:number, annualInvestment:number, expectedReturn:number, duration:number}) {
+    const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+    const annualData = []
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i+1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+    return annualData;
+  }
 }
